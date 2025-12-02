04d1cc493a55bbdb1ec2c5aadbeff0da58a1bf39
htetaunglinn-dev <htaunglin@gmail.com>
2025-12-02 23:33:59 +0700

    update(): mobile responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -318,7 +318,7 @@ export default function Home() {
                 </div>
 
                 {/* Technical Indicators */}
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {!indicators ? (
                     <>
                       {[...Array(4)].map((_, i) => (
